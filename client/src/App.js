335c0db3bfd708dedc8f6382a38b413f75e41330
necelentano <necelentano@gmail.com>
2022-04-27 20:15:11 +0300

    4. React Login Authentication with JWT Access, Refresh Tokens, Cookies

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,13 @@ import RequireAuth from './components/RequireAuth';
 import { Routes, Route } from 'react-router-dom';
 
 const ROLES = {
-  User: 2001,
-  Editor: 1984,
-  Admin: 5150,
-};
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+}
 
 function App() {
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -35,14 +36,16 @@ function App() {
         <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
           <Route path="editor" element={<Editor />} />
         </Route>
+
+
         <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path="admin" element={<Admin />} />
         </Route>
-        <Route
-          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
-        >
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
           <Route path="lounge" element={<Lounge />} />
         </Route>
+
         {/* catch all */}
         <Route path="*" element={<Missing />} />
       </Route>
