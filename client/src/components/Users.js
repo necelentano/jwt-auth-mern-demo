c8478688a729c020a8921565612f5ca66d816f69
necelentano <necelentano@gmail.com>
2022-09-07 22:57:37 +0300

    Multiple Device/Login Support & Bug fix on client

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -3,49 +3,51 @@ import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const Users = () => {
-    const [users, setUsers] = useState();
-    const axiosPrivate = useAxiosPrivate();
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [users, setUsers] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-        let isMounted = true;
-        const controller = new AbortController();
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
 
-        const getUsers = async () => {
-            try {
-                const response = await axiosPrivate.get('/users', {
-                    signal: controller.signal
-                });
-                const userNames = response.data.users.map(user => user.username)
-                console.log(response.data);
-                isMounted && setUsers(userNames);
-            } catch (err) {
-                console.error(err);
-                navigate('/login', { state: { from: location }, replace: true });
-            }
-        }
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get("/users", {
+          signal: controller.signal,
+        });
+        const userNames = response.data.map((user) => user.username);
+        console.log(response.data);
+        isMounted && setUsers(userNames);
+      } catch (err) {
+        console.error(err);
+        navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
 
-        getUsers();
+    getUsers();
 
-        return () => {
-            isMounted = false;
-            controller.abort();
-        }
-    }, [])
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
 
-    return (
-        <article>
-            <h2>Users List</h2>
-            {users?.length
-                ? (
-                    <ul>
-                        {users.map((user, i) => <li key={i}>{user}</li>)}
-                    </ul>
-                ) : <p>No users to display</p>
-            }
-        </article>
-    );
+  return (
+    <article>
+      <h2>Users List</h2>
+      {users?.length ? (
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users to display</p>
+      )}
+    </article>
+  );
 };
 
 export default Users;
